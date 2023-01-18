dc2b6d117a895041782810bea0d460c1721a88b4
tRomanova <66643712+tRomanova@users.noreply.github.com>
2023-01-18 12:17:32 -0600

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,11 +1,22 @@
-function displayMessage(params) {
-    let msg = document.getElementById("message").value;
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: "AppName",
-            text: msg
-        }
-    )
+//entering function
+function getValues() {
+    let inputSting = document.getElementById('userString').getValues
+
+    let rverseString = reverseString(inputSting);
+    displayString(rverseString);
+}
+
+//logic function
+// reverse a string
+function reverseString(userString) {
+    //reverse the string
+
+    return ;
+}
+
+//view function
+function displayString(revString) {
+    document.getElementById('results').textContent = revString;
+    document.getElementById('alert').classList.remove('invisible');
 }
